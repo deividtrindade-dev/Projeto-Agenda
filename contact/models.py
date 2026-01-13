20d397e69ad22e4a948f211cee0c82026569b8d1
deividtrindade-dev <deivid.trindade@wtmdobrasil.com>
2026-01-13 15:25:18 -0300

    Configurando os campos e widgets do formulário

--- contact/models.py
+++ contact/models.py
@@ -14,9 +14,9 @@ class Category(models.Model):
         return self.name
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50,)
     last_name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50,)
     email = models.EmailField(max_length=254, blank=True)
     create_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
