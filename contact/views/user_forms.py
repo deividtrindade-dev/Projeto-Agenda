a834c0ac3981e56e74346338e98965b5ca4083eb
deividtrindade-dev <deivid.trindade@wtmdobrasil.com>
2026-01-21 14:57:44 -0300

    login_required - garantindo que o usuário está logado para acessar view

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
 from django.contrib import auth, messages
 from contact.forms import RegisterForm, RegisterUpdateForm
-
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     form = RegisterForm()
@@ -23,7 +23,7 @@ def register(request):
         }
     )
 
-
+@login_required(login_url='contact:login')
 def user_update(request):
     form = RegisterUpdateForm(instance=request.user)
     if request.method == "POST":
@@ -65,6 +65,7 @@ def login_view(request):
         }
     )
 
-
+@login_required(login_url='contact:login')
 def logout_view(request):
     auth.logout(request)
+    return redirect('contact:login')
